5f6602fdef963bd9da41f0584b85198602f2fe50
Volodymyr Denshchykov <volodymyr@clevertech.biz>
2014-02-27 19:19:35 +0200

    added config for travis tests

--- models/orm/FileModel.js
+++ models/orm/FileModel.js
@@ -27,6 +27,12 @@ module.exports = function ( sequelize, DataTypes ) {
                 type: DataTypes.DATE,
                 allowNull: false,
                 defaultValue: DataTypes.NOW
+            },
+            UserId: {
+                type: DataTypes.INTEGER
+            },
+            AccountId: {
+                type: DataTypes.INTEGER
             }
         },
         {
--- module.js
+++ module.js
@@ -2,4 +2,4 @@ var Module = require( 'classes' ).ModuleClass.extend( {
 
 } );
 
-module.exports = new Module( 'clever-file', injector );
+module.exports = new Module( 'clever-upload-backend', injector );
